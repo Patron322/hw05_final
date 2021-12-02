5855b3622122cf39af6799cd9c9bc55617e24f58
Алексей Бахмутов <ququ.kuku@yandex.ru>
2021-12-02 22:51:52 +0500

    v1.2

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -10,9 +10,7 @@ class PostForm(forms.ModelForm):
         fields = ('text', 'group', 'image')
 
 
-
 class CommentForm(forms.ModelForm):
-
     class Meta:
         model = Comment
         fields = ('text',)
